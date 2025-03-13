0e8e005463c75c74b3be8b60d50db7dd8c5ee573
SeanTheJedi <richmondgyimah5@gmail.com>
2025-03-13 00:21:12 -0400

    getting contact form values

---
+++ app.js
@@ -0,0 +1,5 @@
+const getContactData = () => {
+    let nameValue = document.getElementById("name").value
+    let emailValue = document.getElementById("email").value
+    let msgValue = document.getElementById("msg").value
+}
